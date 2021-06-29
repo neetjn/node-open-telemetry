3ce12265f388e7f6c583d5050d74a2eacf308c46
John Nolette <john.nolette@catapultsports.com>
2021-06-29 10:59:55 -0400

    Adding locust

---
+++ load/locustfile.py
@@ -0,0 +1,16 @@
+import time
+from locust import HttpUser, task, between
+
+class QuickstartLoad(HttpUser):
+  wait_time = between(1, 2.5)
+
+  @task
+  def view_todo_collection(self):
+    self.client.get('/todo')
+
+  @task(3)
+  def view_todo_items(self):
+    for item_id in range(10):
+      self.client.get(f'/todo/{item_id}', name'=/todo')
+      time.sleep(1)
+
